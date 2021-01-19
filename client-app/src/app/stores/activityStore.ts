a965a071cc93a7d797eec7d10cee3a91f85ecba9
nikcar <nikcar34@gmail.com>
2021-01-19 17:47:48 +0100

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -37,8 +37,24 @@ class ActivityStore {
   target = "";
 
   get activitiesByDate() {
-    const activities = Array.from(this.activityRegistry.values());
-    return activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+    return Object.entries(
+      sortedActivities.reduce((activities, activity) => {
+        const date = activity.date.split("T")[0];
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: IActivity[] })
+    );
   }
 
   loadActivities = async () => {
